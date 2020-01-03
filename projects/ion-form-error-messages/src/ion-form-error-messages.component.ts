7cca8791168a47fec64774a6a0ab1c558e2748f8
Rodrigo Villaca <rvillaca@drfirst.com>
2020-01-03 12:50:05 -0800

    1.0.3

--- projects/ion-form-error-messages/src/ion-form-error-messages.component.ts
+++ projects/ion-form-error-messages/src/ion-form-error-messages.component.ts
@@ -4,16 +4,14 @@ import { FormGroup } from '@angular/forms';
 @Component({
     selector: 'ion-form-error-messages',
     template: `
-        <ion-item *ngIf="isAnyControlInvalid()">
+        <ion-item *ngIf="isAnyControlInvalid()" lines="none">
             <ion-grid class="ion-no-padding">
-                <ng-container *ngFor="let controlKey of objectToKeys(formGroup?.controls)">
+                <ng-container *ngFor="let controlKey of filteredControls">
                     <ng-container *ngFor="let errorKey of objectToKeys(getErrors(controlKey))">
                         <ng-container *ngIf="messages[controlKey] || {} as errorMessages">
                             <ion-row *ngIf="errorMessages[errorKey]">
                                 <ion-col class="ion-no-padding ion-text-wrap">
-                                    <ion-text [color]="color">
-                                        {{ errorMessages[errorKey] }}
-                                    </ion-text>
+                                    <ion-text [color]="color" [innerHtml]="errorMessages[errorKey]"> </ion-text>
                                 </ion-col>
                             </ion-row>
                         </ng-container>
@@ -22,13 +20,35 @@ import { FormGroup } from '@angular/forms';
             </ion-grid>
         </ion-item>
     `,
-    styles: []
+    styles: [' ion-item { --min-height: 16px; font-size: 14px;}']
 })
 export class IonFormErrorMessagesComponent implements OnInit {
     @Input() formGroup: FormGroup;
     @Input() messages: { [control: string]: { [error: string]: string } };
     @Input() color = 'danger';
+    @Input() control: string;
+    @Input() controls: string[];
 
+    get filteredControls() {
+        if (!this.formGroup) {
+            return [];
+        }
+        let controls = this.objectToKeys(this.formGroup.controls);
+        if (controls && (this.controls || this.control)) {
+            let filterToUse: string[] = [];
+            if (this.control) {
+                filterToUse.push(this.control);
+            }
+            if (Array.isArray(this.controls)) {
+                filterToUse = filterToUse.concat(this.controls);
+            }
+
+            if (filterToUse.length) {
+                controls = controls.filter(control => filterToUse.indexOf(control) >= 0);
+            }
+        }
+        return controls;
+    }
     constructor() {}
 
     ngOnInit() {}
@@ -71,7 +91,7 @@ export class IonFormErrorMessagesComponent implements OnInit {
         }
     }
 
-    objectToKeys(value: any): any {
+    objectToKeys(value: any): string[] {
         if (!value) {
             return value;
         }
